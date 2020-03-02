a96eecef5dda80a44c95f5a57c883c46fd5585e3
nobody5050 <32989720+nobody5050@users.noreply.github.com>
2020-03-01 22:40:30 -0600

    arcade physics

--- scripts/index.js
+++ scripts/index.js
@@ -8,7 +8,13 @@ const config = {
     create: create,
     update: update
   }
-
+  physics: {
+        default: 'arcade',
+        arcade: {
+            gravity: { y: 500 }, // will affect our player sprite
+            debug: false // change if you need
+        }
+    },
 }
 const game = new Phaser.Game(config);
 
